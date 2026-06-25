ee6bdbeb0b99da1c5433a004d6a7adc66f424b36
sumitrajput-04 <sumitrajput8668@fmail.com>
2026-06-25 21:00:51 +0530

    Day 25

---
+++ Day 25/Q3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name[5][50], temp[50];
+    int i, j;
+
+    printf("Enter 5 names:\n");
+    for(i = 0; i < 5; i++)
+        scanf("%s", name[i]);
+
+    for(i = 0; i < 4; i++) {
+        for(j = i + 1; j < 5; j++) {
+            if(strcmp(name[i], name[j]) > 0) {
+                strcpy(temp, name[i]);
+                strcpy(name[i], name[j]);
+                strcpy(name[j], temp);
+            }
+        }
+    }
+
+    printf("Names in alphabetical order:\n");
+    for(i = 0; i < 5; i++)
+        printf("%s\n", name[i]);
+
+    return 0;
+}
---
+++ Day 25/Q4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char word[5][50], temp[50];
+    int i, j;
+
+    printf("Enter 5 words:\n");
+    for(i = 0; i < 5; i++)
+        scanf("%s", word[i]);
+
+    for(i = 0; i < 4; i++) {
+        for(j = i + 1; j < 5; j++) {
+            if(strlen(word[i]) > strlen(word[j])) {
+                strcpy(temp, word[i]);
+                strcpy(word[i], word[j]);
+                strcpy(word[j], temp);
+            }
+        }
+    }
+
+    printf("Words sorted by length:\n");
+    for(i = 0; i < 5; i++)
+        printf("%s\n", word[i]);
+
+    return 0;
+}
